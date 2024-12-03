c9f16ab647038ee28aaa14ac9e29652bfcac40e8
Spinichi <spinichi@naver.com>
2024-12-03 15:33:52 +0900

    createArticleTable r

--- src/test/java/com/ll/simpleDb/SimpleDbTest.java
+++ src/test/java/com/ll/simpleDb/SimpleDbTest.java
@@ -1,6 +1,7 @@
 package com.ll.simpleDb;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.TestMethodOrder;
 
@@ -11,5 +12,21 @@ public class SimpleDbTest {
     @BeforeAll
     public static void beforeAll() {
         simpleDb = new SimpleDb("localhost", "root", "lldj123414", "simpleDb__test");
+    }
+
+    private static void createArticleTable() {
+        simpleDb.run("DROP TABLE IF EXISTS article");
+
+        simpleDb.run("""
+                CREATE TABLE article (
+                    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                    PRIMARY KEY(id),
+                    createdDate DATETIME NOT NULL,
+                    modifiedDate DATETIME NOT NULL,
+                    title VARCHAR(100) NOT NULL,
+                    `body` TEXT NOT NULL,
+                    isBlind BIT(1) NOT NULL DEFAULT 0
+                )
+                """);
     }
 }
